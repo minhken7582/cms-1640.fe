562e5d6f71e73d78ca862cf2bd7fe89ab81936d1
LÊ NGỌC THỌ <lengoctho136@gmail.com>
2024-04-02 15:35:28 +0700

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
 import Image from "next/image";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+
+      <main
+        className="min-h-screen bg-gray-50 dark:bg-gray-900"
+        style={{
+          minHeight: "calc(100vh - 112px)",
+        }}
+      ></main>
+
+      <Footer />
+    </>
+  );
 }
