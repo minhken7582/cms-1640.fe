155511f5d402a3eb32697c9a228b6f2b8e31164d
LÊ NGỌC THỌ <lengoctho136@gmail.com>
2024-04-04 22:45:31 +0700

    student profile

--- src/app/web/student/profile/page.tsx
+++ src/app/web/student/profile/page.tsx
@@ -1,1 +1,52 @@
- 
+"use client";
+
+import React from "react";
+import MainLayout from "@/components/MainLayout";
+
+export default function Profile() {
+  return (
+    <MainLayout>
+      <h1 className="text-xl font-medium leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white mb-5">
+        Student profile
+      </h1>
+
+      <section className="bg-white overflow-hidden shadow rounded-lg border">
+        <div className="border-gray-200 px-4 py-5 sm:p-0">
+          <dl className="sm:divide-y sm:divide-gray-200">
+            <div className="py-3 sm:py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Student Name
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                John Doe
+              </dd>
+            </div>
+
+            <div className="py-3 sm:py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Student ID</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                johndoe@example.com
+              </dd>
+            </div>
+
+            <div className="py-3 sm:py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">
+                Student Email
+              </dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                johndoe@example.com
+              </dd>
+            </div>
+
+            <div className="py-3 sm:py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Faculty</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                123 Main St Anytown, USA 12345
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </section>
+    </MainLayout>
+  );
+}
